f5202bb47548c99469ea3c3cdad4f483a868b127
dkiran1209 <dkiran1209@gmail.com>
2017-06-02 06:30:22 +0530

    Add files via upload

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://grocery.clientdemoproject.com/admin/');
+define('HTTP_CATALOG', 'http://grocery.clientdemoproject.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://grocery.clientdemoproject.com/admin/');
+define('HTTPS_CATALOG', 'http://grocery.clientdemoproject.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/upload/admin/');
+define('DIR_SYSTEM', '/var/www/html/upload/system/');
+define('DIR_IMAGE', '/var/www/html/upload/image/');
+define('DIR_LANGUAGE', '/var/www/html/upload/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/upload/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/upload/system/config/');
+define('DIR_CACHE', '/var/www/html/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/upload/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencartuser');
+define('DB_PASSWORD', 'welcome1');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
